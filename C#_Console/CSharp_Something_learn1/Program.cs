7a2cb0320077d1d2e832cbaa8c5dd8198a03acaf
waixxt3213 <qnqn60360@gmail.com>
2021-07-14 18:40:23 +0900

    C#_learn project_1 create

---
+++ C#_Console/CSharp_Something_learn1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Something_learn1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int[] num1 = new int[] {1, 2, 3, 4};
+            string[] str1 = new string[] {"One", "Two", "Three", "Four"};
+            */
+            int[] num2 = {1, 2, 3, 4};
+            string[] str2 = {"One", "Two", "Three", "Four"};
+            foreach (int i in num2)
+            {
+                System.Console.Write("{0} ", i);
+            }
+            System.Console.Write("\n");
+            foreach (string str in str2)
+            {
+                System.Console.Write("{0} ", str);
+            }
+        }
+    }
+}
